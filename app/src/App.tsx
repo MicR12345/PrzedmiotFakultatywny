03e82b865901a3bdb5e2cb0dd1b808428a652ba8
MiloszProc <miloszproc99@gmail.com>
2021-05-17 18:17:30 +0200

    notatki 17.05

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {Switch,Route,BrowserRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-import Movie from './views/movie/Movie'
-import Home from './views/home/Home'
-import Page404 from './views/page404/Page404'
+import Movie from './views/movie/Movie';
+import Home from './views/home/Home';
+import Page404 from './views/page404/Page404';
+import store from './store';
 
 import './App.css';
 
@@ -12,11 +14,13 @@ const  App= () => {
   return (
     <div className="App">
       <BrowserRouter>
-        <Switch>
-          <Route path="/movie/:id" component={Movie}/>
-          <Route path="/" component={Home} exact/>
-          <Route path="*" component={Page404}/>
-        </Switch>
+        <Provider store={ }>
+          <Switch>
+             <Route path="/movie/:id" component={Movie}/>
+             <Route path="/" component={Home} exact/>
+             <Route path="*" component={Page404}/>
+          </Switch>
+        </Provider>
       </BrowserRouter>
     </div>
   );
