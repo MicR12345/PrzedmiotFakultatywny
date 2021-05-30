cda6dff97d20ffcaca9f489e379ecdbc0c2701a6
Koksuvi <46352534+MicR12345@users.noreply.github.com>
2021-05-30 17:49:47 +0200

    Milestones

--- yarrrr/src/views/milestones/Milestones.tsx
+++ yarrrr/src/views/milestones/Milestones.tsx
@@ -3,6 +3,8 @@ import NavPanel from '../../components/NavPanel';
 import { makeStyles } from '@material-ui/core/styles';
 import Milestone from '../../components/Milestone';
 import background from '../../img/Background2.jpg';
+import { MilestoneDefault } from '../../defaults/Milestones';
+import ClickCounterDefault from '../../defaults/DefaultClickCounter';
 
 const useStyles = makeStyles({
     Milestones:{
@@ -20,6 +22,54 @@ const useStyles = makeStyles({
 
 const Milestones:React.FC = () =>{
     const classes = useStyles();
+    const CheckAcheivementCondiditons = () =>{
+      if(!window.localStorage.getItem('ClickCounter'))window.localStorage.setItem('ClickCounter',JSON.stringify(ClickCounterDefault));
+      const ClickCounter = JSON.parse(window.localStorage.getItem('ClickCounter') || JSON.stringify(ClickCounterDefault));
+      let Milestones = JSON.parse(window.localStorage.getItem('Milestones') || JSON.stringify(MilestoneDefault));
+      if(ClickCounter.count>=10000){
+          Milestones.Milestone[1]=1;
+      }
+      if(ClickCounter.count>=50000){
+          Milestones.Milestone[2]=1;
+      }
+      if(
+          ClickCounter.passiveClicker[1] >= 10 &&
+          ClickCounter.passiveClicker[2] >= 10 &&
+          ClickCounter.passiveClicker[3] >= 10 &&
+          ClickCounter.passiveClicker[4] >= 10 &&
+          ClickCounter.passiveClicker[5] >= 10 &&
+          ClickCounter.passiveClicker[6] >= 10 &&
+          ClickCounter.passiveClicker[7] >= 10
+      ){
+          Milestones.Milestone[3]=1;
+      }
+      else Milestones.Milestone[3]=0;
+      if(ClickCounter.passiveClicker[1] >= 100){
+          Milestones.Milestone[4]=1;
+      }
+      else Milestones.Milestone[4]=0;
+      if(ClickCounter.count>=100000){
+          Milestones.Milestone[5]=1;
+      }
+      if(
+          ClickCounter.passiveClicker[1] >= 50 &&
+          ClickCounter.passiveClicker[2] >= 50 &&
+          ClickCounter.passiveClicker[3] >= 50 &&
+          ClickCounter.passiveClicker[4] >= 50 &&
+          ClickCounter.passiveClicker[5] >= 50 &&
+          ClickCounter.passiveClicker[6] >= 50 &&
+          ClickCounter.passiveClicker[7] >= 50
+      ){
+          Milestones.Milestone[6]=1;
+      }
+      else Milestones.Milestone[6]=0;
+      if(ClickCounter.passiveClicker[7] >= 100){
+          Milestones.Milestone[7]=1;
+      }
+      else Milestones.Milestone[7]=0;
+      window.localStorage.setItem('Milestones',JSON.stringify(Milestones));
+  }
+  CheckAcheivementCondiditons();
     return(
         <div style ={{ backgroundImage: `url(${background})` }}>
             <NavPanel/>
@@ -30,7 +80,7 @@ const Milestones:React.FC = () =>{
               <Milestone milestoneNumber={4} name='Prawdziwy degenerat' description="Ulepsz pierwszego pasywnego clickera do 100 poziomu." />
               <Milestone milestoneNumber={5} name='Wróg Kompani Wschodnioindyjskiej' description="Posiadaj co najmniej 100000 monet." />
               <Milestone milestoneNumber={6} name='Stopy wody pod kilem' description="Ulepsz wszystkie pasywne clickery do co najmniej 50 poziomu." />
-              <Milestone milestoneNumber={6} name='Król Piratów' description="Ulepsz ostatniego pasywnego clickera na 100 poziom." />
+              <Milestone milestoneNumber={7} name='Król Piratów' description="Ulepsz ostatniego pasywnego clickera na 100 poziom." />
             </div>
         </div>);
 }
